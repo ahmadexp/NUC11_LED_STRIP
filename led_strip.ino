866b00cad62176588a5c5c99fbc520f33ebb4480
Ahmad Byagowi <ahmadexp@gmail.com>
2022-05-29 12:21:42 -0700

    Update led_strip.ino

--- led_strip.ino
+++ led_strip.ino
@@ -1,24 +1,29 @@
-
 #include <FastLED_NeoPixel.h>
 
 // LED MAX between 0 to 255
-#define MAX 50
+#define STRIP_MAX 100
+#define SKULL_MAX 255
 
-FastLED_NeoPixel<10, 6, NEO_GRB> front;     
+FastLED_NeoPixel<10, 6, NEO_GRB> io;     
 FastLED_NeoPixel<20, 7, NEO_GRB> left; 
-FastLED_NeoPixel<20, 8, NEO_GRB> right;      
+FastLED_NeoPixel<20, 8, NEO_GRB> right;
+FastLED_NeoPixel<20, 9, NEO_GRB> skull;      
      
 
 void setup() {
   
-	front.begin();  
-	front.setBrightness(MAX);
+  io.begin();  
+  io.setBrightness(STRIP_MAX);
 
   right.begin();  
-  right.setBrightness(MAX);
+  right.setBrightness(STRIP_MAX);
 
   left.begin(); 
-  left.setBrightness(MAX);
+  left.setBrightness(STRIP_MAX);
+
+  skull.begin(); 
+  skull.setBrightness(SKULL_MAX);
+
 }
 
 float t=0;
@@ -28,24 +33,30 @@ void loop() {
   else
     t=0;
  
-	front.setPixelColor((int)(((sin(t)+1)*2.5)), front.Color(255, 255, 255)); 
-	front.show();
+  io.setPixelColor((int)(((sin(t)+1)*2.5)), io.Color(255, 255, 255)); 
+  io.show();
   
   right.setPixelColor((int)(((sin(t)+1)*7.5)), right.Color(255, 255, 255));  
   right.show();
 
   left.setPixelColor((int)(((sin(t)+1)*7.5)), left.Color(255, 255, 255));  
   left.show();
- 
-	delay(1); //in milliseconds
 
-  front.setPixelColor((int)(((sin(t)+1)*2.5)), front.Color(0, 0, 0));  
-  front.show();
+  skull.setPixelColor((int)(((sin(t)+1)*5)), skull.Color(255, 255, 255));  
+  skull.show();
+ 
+  delay(1); //in milliseconds
+
+  io.setPixelColor((int)(((sin(t)+1)*2.5)), io.Color(0, 0, 0));  
+  io.show();
   
   right.setPixelColor((int)(((sin(t)+1)*7.5)), right.Color(0, 0, 0));  
   right.show();
 
   left.setPixelColor((int)(((sin(t)+1)*7.5)), left.Color(0, 0, 0)); 
   left.show();
+
+  skull.setPixelColor((int)(((sin(t)+1)*5)), skull.Color(0, 0, 0));  
+  skull.show();
   
 }
